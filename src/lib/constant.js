a76f967b7dc5498816734daf7d323cba881f6a7f
gauri dubey <gauri.dubey1012@gmail.com>
2024-03-11 22:09:38 +0530

    implementation of jumble words and time second algo

--- src/lib/constant.js
+++ src/lib/constant.js
@@ -2,5 +2,7 @@ export const AlgorithmType = {
     RANDOMLETTER : "randomLetter",
     FIRSTLETTER: "firstLetter",
     LASTLETTER: "lastLetter",
-    REMOVEWORD: "removeWord"
+    REMOVEWORD: "removeWord",
+    JUMBLEWORD: "jumbleWords",
+    TIME_SECOND_ALGO: "timeSecondVerse"
 }
